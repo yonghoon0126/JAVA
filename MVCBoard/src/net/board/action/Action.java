c5ad62a5d153966037303ad2571fa9e380c7dfcd
yonghoon0126 <93071938+yonghoon0126@users.noreply.github.com>
2021-12-29 17:10:31 +0900

    Delete MVCBoard directory

--- MVCBoard/src/net/board/action/Action.java
+++
@@ -1,7 +0,0 @@
-package net.board.action;
-
-import javax.servlet.http.*;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
-}
